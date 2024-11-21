502c5da14d41266c0d2ddd4ad6527c1b52ec7b57
ADRILOL2 <aavila20479@iesjoanramis.org>
2024-11-21 17:34:51 +0100

    Cambio en rama "feature"

--- src/java/CalculadoraSimple.java
+++ src/java/CalculadoraSimple.java
@@ -1,6 +1,12 @@
 package java;
 import java.util.Scanner;
 public class CalculadoraSimple {
+public static double dividir(double a, double b) {
+return a / b;
+}
+public static double multiplicar(double a, double b) {
+return a * b;
+}
 public static double sumar(double a, double b) {
 return a + b;
 }
@@ -13,11 +19,15 @@ System.out.println("Calculadora Simple:");
 System.out.print("Introduce el primer número: ");
 double numero1 = scanner.nextDouble();      
 System.out.print("Introduce el segundo número: ");
-double numero2 = scanner.nextDouble();      
-double resultadoSuma = sumar(numero1, numero2);
-double resultadoResta = restar(numero1, numero2);
-System.out.println("Resultado de la suma: " + resultadoSuma);
-System.out.println("Resultado de la resta: " + resultadoResta);
+double numero2 = scanner.nextDouble();
+double resultadoDividir = dividir(numero1, numero2);
+double resultadoSumar = sumar(numero1, numero2);
+double resultadoRestar = restar(numero1, numero2);
+double resultadoMultiplicar = multiplicar(numero1, numero2);
+System.out.println("Resultado de la suma: " + resultadoSumar);
+System.out.println("Resultado de la resta: " + resultadoRestar);
+System.out.println("Resultado de la division: " + resultadoDividir);
+System.out.println("Resultado de la multiplicación: " + resultadoMultiplicar);
 scanner.close();
 }
 }
